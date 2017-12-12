89537ea9d5bdc78f400ce8e74c0554b67b2b624a
japer21 <17979719+japer21@users.noreply.github.com>
2017-12-12 18:25:44 +0100

    la nouvelle partie

---
+++ SmartFridge/app/src/main/java/mbds/smartfridge/ActivityProduits.java
@@ -0,0 +1,70 @@
+package mbds.smartfridge;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import mbds.smartfridge.model.Frigo;
+import mbds.smartfridge.model.Produit;
+import mbds.smartfridge.utils.ProduitAdapterHelper;
+
+public class ActivityProduits extends AppCompatActivity {
+
+    ListAdapter adapter = null;
+    ListView listProds;
+    TextView count;
+    ArrayList<Produit> LP = new ArrayList<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_produits);
+        listProds=(ListView)findViewById(R.id.produits);
+        initList();
+        adapter=new ProduitAdapterHelper(this,LP);
+        listProds.setAdapter(adapter);
+        listProds.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Produit p=(Produit) adapter.getItem(position);
+                Bundle args = new Bundle();
+                args.putSerializable("produit",p);
+                Intent i = new Intent(ActivityProduits.this,ActivityProduit.class);
+                i.putExtras(args);
+                startActivity(i);
+            }
+        });
+        count=(TextView)findViewById(R.id.countProd);
+        count.setText(adapter.getCount()+" Produits");
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+    public void initList(){
+        LP.add(new Produit(1,1,"Oeufs","Cat1"));
+        LP.add(new Produit(2,1,"Beurre","Cat2"));
+        LP.add(new Produit(3,2,"Fromage","Cat2"));
+        LP.add(new Produit(4,2,"Lait","Cat2"));
+        LP.add(new Produit(5,3,"Jambon","Cat3"));
+        LP.add(new Produit(6,3,"Lait entier","Cat3"));
+        LP.add(new Produit(7,1,"Beurre doux","Cat2"));
+    }
+
+}
---
+++ SmartFridge/app/src/main/java/mbds/smartfridge/utils/FrigoAdapterHelper.java
@@ -0,0 +1,136 @@
+package mbds.smartfridge.utils;
+
+import android.content.Context;
+import android.database.DataSetObserver;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import mbds.smartfridge.R;
+import mbds.smartfridge.model.Frigo;
+
+/**
+ * Created by jperk on 03/12/2017.
+ */
+
+public class FrigoAdapterHelper implements ListAdapter {
+    ArrayList<Frigo> LF;
+    Context ctx;
+    TextView frigoName ;
+    TextView name;
+    TextView frigoStatus;
+    ImageView frigo;
+    //Button bConnect;
+    //Button bDisconnect;
+    public FrigoAdapterHelper(Context ctx) {
+        this.LF = new ArrayList<>();
+        LF.add(new Frigo(1,"Samsung SMARTF","Connected",4,50,"decomp","samsung garantie 6a","180cm x 50cm x 60cm","Samsung"));
+        LF.add(new Frigo(2,"LG SMRTF","Disconnected",5,50,"decomp","lg garantie 5a","180cm x 50cm x 60cm","LG"));
+        LF.add(new Frigo(3,"BEKO FSMRT","Connected",6,50,"decomp","beko garantie 2a","180cm x 50cm x 60cm","Beko"));
+        this.ctx = ctx;
+    }
+
+    public FrigoAdapterHelper(ArrayList<Frigo> LF, Context ctx) {
+        super();
+        this.LF = LF;
+        this.ctx = ctx;
+    }
+
+    @Override
+    public boolean areAllItemsEnabled() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled(int position) {
+        return true;
+    }
+
+    @Override
+    public void registerDataSetObserver(DataSetObserver observer) {
+
+    }
+
+    @Override
+    public void unregisterDataSetObserver(DataSetObserver observer) {
+
+    }
+
+    @Override
+    public int getCount() {
+        return LF.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return LF.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        View returnView;
+        if(convertView==null){
+
+            returnView=View.inflate(ctx, R.layout.frigo, null);
+        }
+        else {
+            returnView = convertView;
+        }
+
+        frigoName=(TextView)returnView.findViewById(R.id.frigoName);
+        frigoName.setText(LF.get(position).getFrigoName());
+        frigoStatus=(TextView)returnView.findViewById(R.id.etatFrigo);
+        frigoStatus.setText(LF.get(position).getFrigoStatus());
+        frigo=(ImageView)returnView.findViewById(R.id.imageID);
+        //frigo.setImageBitmap(LF.get(position).getImage());
+        name=(TextView)returnView.findViewById(R.id.name);
+        name.setText(LF.get(position).getNom());
+       /* bDisconnect=(Button)returnView.findViewById(R.id.buttonDeconnecter);
+        bConnect=(Button)returnView.findViewById(R.id.buttonConnecter);
+        if(LF.get(position).getFrigoStatus()=="Connected"){
+
+            bDisconnect.setVisibility(View.VISIBLE);
+            bConnect.setVisibility(View.INVISIBLE);
+
+        }
+        if(LF.get(position).getFrigoStatus()=="Disconnected"){
+            bDisconnect.setVisibility(View.INVISIBLE);
+            bConnect.setVisibility(View.VISIBLE);
+        }
+*/
+        return returnView;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return 0;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 1;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        if(LF.isEmpty())
+            return true;
+        else
+            return false;
+    }
+}
